8e71e434b92e3ea13b51a74de8370b504697ae44
Isaiah <isaiahdj@gmail.com>
2021-07-06 19:07:17 -0700

    Basic JavaScript

---
+++ JavaScript/main.js
@@ -0,0 +1,17 @@
+
+function myFunction() {
+    var str1 = "Hello "; // Asigns a string var to str1
+    var str2 = "world!"; // Asigns a string var to str2
+    var res = str1.concat(str2); // Concatonates str1 with str2 to get a new string and put into var res
+    document.getElementById("demo").innerHTML = res;
+}
+
+window.alert("hello world")//displays an alert window 
+document.write("\"Today you are you!"
++" That is truer than true!"
++" There is no one alive who is you-er than you!\""// displays dr.seuss quote on screen
++"-Dr.Seuss")
+var colRed=" This is writen in red. "//defines variable colRed
+var colRed=colRed.fontcolor("red")//gives variable colRed a red text color
+document.write(colRed)//calls variable colRed and prints it on screen
+document.write(3+3+3)//adds 3+3 and prints the sum (9) on screen
