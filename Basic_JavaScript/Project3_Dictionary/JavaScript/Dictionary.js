4e682a2881ceb1c3c4016f22a32708f1b362fe3f
Isaiah <isaiahdj@gmail.com>
2021-07-07 09:39:01 -0700

    Adding Delete in Dictionary

--- Basic_JavaScript/Project3_Dictionary/JavaScript/Dictionary.js
+++ Basic_JavaScript/Project3_Dictionary/JavaScript/Dictionary.js
@@ -6,6 +6,7 @@ function my_Dictionary() { // This makes my first dictionary
         Age: 13,    //dictionary index
         Sound: "oink"   //dictionary index
     };
+    delete Animal.Color; //Deletes the value of animal.color
     //The line below gets all the elements in the dictionary and prints them on a new line
     document.getElementById("Dictionary").innerHTML = Animal.Species + "<br>" + Animal.Color + "<br>" + Animal.Sound + "<br>" + Animal.Age;
 }
